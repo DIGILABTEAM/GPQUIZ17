4f29bd6bdda34833fb216850573a817b7de9bbcc
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-12-28 11:49:03 +0530

    Questions changes

--- js/questions.js
+++ js/questions.js
@@ -3,44 +3,44 @@ var data =
 	[
 		[
 			{
-				question: "Which of the following would be used as a rescue inhaler in case of asthma emergency?",
-				option1: "Budesonide",
-				option2: "Fluticasone furoate",
-				option3: "Albuterol sulfate",
-				option4: "A and C",
-				answer: 3
-			},
-			{
-				question: "The type of emphysema associated with smoking is",
-				option1: "Panacinar",
-				option2: "Centriacinar",
-				option3: "Distal acinar",
-				option4: "Irregular",
+				question: "COPD patient with pulmonary hypertension and acute RHF. Which is TRUE?",
+				option1: "100% oxygen will decrease the pulmonary artery pressure",
+				option2: "Sildenafil will be useful for treating RHF",
+				option3: "Noradrenaline is an appropriate inotrope for this patient",
+				option4: "If an inotrope is required, Milronone would not be useful.",
 				answer: 2
 			},
 			{
-				question: "In chronic bronchitis",
-				option1: "The hallmark is hypersecretion of mucous in the large airways",
-				option2: "There is a marked increase in goblet cells in the main bronchi",
-				option3: "Infection is a primary cause",
-				option4: "Cigarette smoke stimulates alveolar leukocytes",
-				answer: 1
-			},
-			{
-				question: "In bronchial asthma",
-				option1: "Extrinsic asthma is initiated by diverse non-immune mechanisms",
-				option2: "Sub-epithelial vagal receptors in respiratory mucosa are insensitive to irritants",
-				option3: "Bronchial wall smooth muscle is atrophic",
-				option4: "Primary mediators include eosinophilic and neutrophilic chemotactic factors",
+				question: "Unilateral diaphragmatic paralysis causes",
+				option1: "no change in total lung capacity",
+				option2: "A decrease in forced vital capacity",
+				option3: "No change in maximum voluntary ventilation",
+				option4: "A decrease in inspiratory reserve volume",
 				answer: 4
 			},
 			{
-				question: "In bacterial pneumonia",
-				option1: "Patchy consolidation of the lung is the dominant feature of bronchopneumonia",
-				option2: "A lobar distribution is a function of anatomical variations",
-				option3: "Klebsiella pneumonia is most common virulent agent",
-				option4: "Alveolar clearance of bacteria is achieved by lymphocytes",
-				answer: 1
+				question: "Asbestos exposure is typically associated with each of the following EXCEPT",
+				option1: "Pleural effusions",
+				option2: "Mesothelioma of the pleura",
+				option3: "Laryngeal cancer",
+				option4: "All of the above",
+				answer: 4
+			},
+			{
+				question: "What percentage of carbon dioxide in the blood is transported bound to hemoglobin?",
+				option1: "90%",
+				option2: "40%",
+				option3: "20%",
+				option4: "5%",
+				answer: 4
+			},
+			{
+				question: "30.	The volume of air that can be exhaled after normal exhalation is the",
+				option1: "Tidal volume",
+				option2: "Residual volume",
+				option3: "Inspiratory reserve volume",
+				option4: "Expiratory reserve volume",
+				answer: 4
 			}
 		]
 	];
