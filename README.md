688195f4c86324b64499f0ca3c43ee0c05b8f3d7
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-12-28 11:41:26 +0530

    deploy
    project release V.17

---
+++ README.md
@@ -0,0 +1,3 @@
+<p>Developed By Digilab</p>
+# GP-QUIZ 
+Developed By Digilab <a href="https://digilabteam.github.io/GPQUIZ16/">GP GAME </a>
---
+++ js/questions.js
@@ -0,0 +1,46 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+				question: "Which of the following would be used as a rescue inhaler in case of asthma emergency?",
+				option1: "Budesonide",
+				option2: "Fluticasone furoate",
+				option3: "Albuterol sulfate",
+				option4: "A and C",
+				answer: 3
+			},
+			{
+				question: "The type of emphysema associated with smoking is",
+				option1: "Panacinar",
+				option2: "Centriacinar",
+				option3: "Distal acinar",
+				option4: "Irregular",
+				answer: 2
+			},
+			{
+				question: "In chronic bronchitis",
+				option1: "The hallmark is hypersecretion of mucous in the large airways",
+				option2: "There is a marked increase in goblet cells in the main bronchi",
+				option3: "Infection is a primary cause",
+				option4: "Cigarette smoke stimulates alveolar leukocytes",
+				answer: 1
+			},
+			{
+				question: "In bronchial asthma",
+				option1: "Extrinsic asthma is initiated by diverse non-immune mechanisms",
+				option2: "Sub-epithelial vagal receptors in respiratory mucosa are insensitive to irritants",
+				option3: "Bronchial wall smooth muscle is atrophic",
+				option4: "Primary mediators include eosinophilic and neutrophilic chemotactic factors",
+				answer: 4
+			},
+			{
+				question: "In bacterial pneumonia",
+				option1: "Patchy consolidation of the lung is the dominant feature of bronchopneumonia",
+				option2: "A lobar distribution is a function of anatomical variations",
+				option3: "Klebsiella pneumonia is most common virulent agent",
+				option4: "Alveolar clearance of bacteria is achieved by lymphocytes",
+				answer: 1
+			}
+		]
+	];
